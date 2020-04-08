f6af2610d57dcd15b7cf166e293c310d3702e6a3
Seth Etter <mail@sethetter.com>
2020-04-08 13:43:36 -0500

    If JSON parsing fails, just pass through raw resp

--- format.go
+++ format.go
@@ -26,23 +26,20 @@ func FormatResponse(res *http.Response) (string, error) {
 		return out, err
 	}
 
-	body, err := FormatBody(res.Header.Get("content-type"), rawBody)
-	if err != nil {
-		return out, err
-	}
+	body := FormatBody(res.Header.Get("content-type"), rawBody)
 	out += body
 
 	return out, nil
 }
 
-func FormatBody(cType string, body []byte) (string, error) {
+func FormatBody(cType string, body []byte) string {
 	if strings.Contains(cType, "application/json") {
 		b := &bytes.Buffer{}
 		if err := json.Indent(b, body, "", "  "); err != nil {
-			return "", err
+			return string(body)
 		}
-		return b.String(), nil
+		return b.String()
 	}
 
-	return string(body), nil
+	return string(body)
 }
